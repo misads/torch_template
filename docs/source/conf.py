08f336bb9e357299ef345410bea50194c70a9f97
misads <523131316@qq.com>
2020-01-05 22:49:44 +0800

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,13 +24,24 @@ author = 'Haoyu Xu'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
+master_doc = "index"
 
 # -- General configuration ---------------------------------------------------
+
+
+autodoc_mock_imports = ["torch", "torchvision", "tqdm", "coloredlogs", "numpy", "visdom"]
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.doctest',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
